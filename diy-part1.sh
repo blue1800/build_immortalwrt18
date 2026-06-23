b2812ebaeeaf8241275e667db4e77cf571c81942
blue1800 <63365195@qq.com>
2026-06-23 21:01:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# 进入源码目录
 cd openwrt
 
-# 替换国内下载源（中科大，解决github超时）
-sed -i 's/https:\/\/github.com/https:\/\/mirror.ghproxy.com\/https:\/\/github.com/g' feeds.conf.default
+# ghproxy 加速github下载，解决拉取插件超时
+sed -i "s#https://github.com#https://mirror.ghproxy.com/https://github.com#g" feeds.conf.default
 
-# 示例：拉取额外插件仓库
-# git clone https://github.com/kenzok8/small-package package/small-package
+# 替换国内源码下载源（中科大）
+sed -i 's#downloads.immortalwrt.org#mirrors.ustc.edu.cn/immortalwrt#g' feeds.conf.default
 
-# 修改内核版本（示例强制6.18）
+# 示例：拉取常用插件仓库，按需注释/开启
+# git clone https://mirror.ghproxy.com/https://github.com/kenzok8/small-package package/small-package
+# git clone https://mirror.ghproxy.com/https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+
+# 强制修改内核版本示例（6.18）
 # sed -i 's/CONFIG_KERNEL_PATCHVER="6.6"/CONFIG_KERNEL_PATCHVER="6.18"/' .config
