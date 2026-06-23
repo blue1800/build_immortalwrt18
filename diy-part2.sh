8b1f9c334ebcd2ab92db3649736523e3c0533466
blue1800 <63365195@qq.com>
2026-06-23 21:05:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,7 +2,7 @@
 # DIY 后置脚本：固定LAN 192.168.100.18 + root密码password + 原版ImmortalWrt Banner
 
 # 1. 修改LAN后台IP为192.168.100.18
-sed -i 's/option ipaddr '\''192.168.1.1'\''/option ipaddr '\''192.168.100.18'\''/' package/base-files/files/etc/config/network
+#sed -i 's/option ipaddr '\''192.168.1.1'\''/option ipaddr '\''192.168.100.18'\''/' package/base-files/files/etc/config/network
 
 # 2. 固化root密码 password 加密串
 ROOT_PWD='$1$openwrt$O9o1O2w0n88qhWuiWdL50.'
